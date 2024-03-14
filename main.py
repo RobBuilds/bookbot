1e2a1493b16697609bece20889e0d1f33e3ba7d4
RobBuilds <152198994+RobBuilds@users.noreply.github.com>
2024-03-14 13:59:04 -0400

    added letter count function

--- main.py
+++ main.py
@@ -1,5 +1,20 @@
 def main():
     with open('books/frankenstein.txt') as f:
-        file_contents = f.read()
-    print(file_contents)
+      file_contents = f.read()
+      print(file_contents)
+      print(word_count(file_contents))
+      print(letter_count(file_contents))
+def word_count(file_contents):
+      words = file_contents.split()
+      return(len(words))
+
+def letter_count(string):
+    string_dict = {}
+    new_string_lower = string.lower()
+    for i in new_string_lower:
+      if i in string_dict:
+        string_dict[i] += 1
+      else:
+        string_dict[i] = 1
+    return string_dict
 main()
